75e7cb8d59e62d35ccec8f393a9e9f7e1bc6e05a
pstutz <philip.stutz@gmail.com>
2011-05-06 08:54:11 +0000

    Preparing new way of configuring compute graphs.

--- src/main/scala/signalcollect/interfaces/ComputeGraph.scala
+++ src/main/scala/signalcollect/interfaces/ComputeGraph.scala
@@ -19,7 +19,10 @@
 
 package signalcollect.interfaces
 
-import signalcollect.api.AsynchronousComputeGraph
+import signalcollect.interfaces.Queue._
+import signalcollect.interfaces.Worker._
+import signalcollect.interfaces.MessageBus._
+import signalcollect.interfaces.Storage._
 
 object ComputeGraph {
   lazy val defaultNumberOfThreads = Runtime.getRuntime.availableProcessors
@@ -31,10 +34,10 @@ trait ComputeGraph extends GraphApi {
 
   def foreach(f: (Vertex[_, _]) => Unit)
   def foreach(f: PartialFunction[Vertex[_, _], Unit])
-  
+
   def countVertices[VertexType <: Vertex[_, _]](implicit m: Manifest[VertexType]): Long
-//  def countEdges: Long
-  
+  //  def countEdges: Long
+
   def sum[N](implicit numeric: Numeric[N]): N
   def product[N](implicit numeric: Numeric[N]): N
   def reduce[ValueType](operation: (ValueType, ValueType) => ValueType): Option[ValueType]
@@ -43,5 +46,59 @@ trait ComputeGraph extends GraphApi {
 
   def setSignalThreshold(t: Double)
   def setCollectThreshold(t: Double)
-  def setStepsLimit(l: Int) 
+  def setStepsLimit(l: Int)
 }
+
+object DefaultConfig extends ComputeGraphConfig
+object DefaultSynchronousConfig extends ComputeGraphConfig(
+  executionMode = SynchronousExecutionMode,
+  workerFactory = Worker.synchronousDirectDeliveryWorkerFactory)
+
+sealed trait ExecutionMode
+object SynchronousExecutionMode extends ExecutionMode
+object AsynchronousExecutionMode extends ExecutionMode
+
+case class ComputeGraphConfig(
+  executionMode: ExecutionMode = AsynchronousExecutionMode,
+  numberOfWorkers: Int = ComputeGraph.defaultNumberOfThreads,
+  workerFactory: WorkerFactory = Worker.defaultFactory,
+  messageInboxFactory: QueueFactory = Queue.defaultFactory,
+  messageBusFactory: MessageBusFactory = MessageBus.defaultFactory,
+  storageFactory: StorageFactory = Storage.defaultFactory,
+  optionalLogger: Option[MessageRecipient[Any]] = None) {
+
+  def withExecutionMode(newExecutionMode: ExecutionMode) = newComputeGraph(newExecutionMode = newExecutionMode)
+  def withNumberOfWorkers(newNumberOfWorkers: Int) = newComputeGraph(newNumberOfWorkers = newNumberOfWorkers)
+  def withWorkerFactory(newNumberOfWorkers: Int) = newComputeGraph(newNumberOfWorkers = newNumberOfWorkers)
+  def withMessageInboxFactory(newMessageInboxFactory: QueueFactory) = newComputeGraph(newMessageInboxFactory = newMessageInboxFactory)
+  def withMessageBusFactory(newMessageBusFactory: MessageBusFactory) = newComputeGraph(newMessageBusFactory = newMessageBusFactory)
+  def withStorageFactory(newStorageFactory: StorageFactory) = newComputeGraph(newStorageFactory = newStorageFactory)
+  def withLogger(logger: MessageRecipient[Any]) = newComputeGraph(newOptionalLogger = Some(logger))
+
+  def newComputeGraph(
+    newExecutionMode: ExecutionMode = executionMode,
+    newNumberOfWorkers: Int = numberOfWorkers,
+    newWorkerFactory: WorkerFactory = workerFactory,
+    newMessageInboxFactory: QueueFactory = messageInboxFactory,
+    newMessageBusFactory: MessageBusFactory = messageBusFactory,
+    newStorageFactory: StorageFactory = storageFactory,
+    newOptionalLogger: Option[MessageRecipient[Any]] = optionalLogger): ComputeGraphConfig = {
+    ComputeGraphConfig(
+      newExecutionMode,
+      newNumberOfWorkers,
+      newWorkerFactory,
+      newMessageInboxFactory,
+      newMessageBusFactory,
+      newStorageFactory,
+      newOptionalLogger)
+  }
+}
+
+
+
+    
+
+
+
+
+
