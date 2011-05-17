60d243a01bd7fdfd07c09b5c3323eb466c88406e
pstutz <philip.stutz@gmail.com>
2011-05-17 09:05:27 +0000

    Direct delivery worker seems to be broken. Changing default worker until we figure this out.

--- src/main/scala/signalcollect/api/Factory.scala
+++ src/main/scala/signalcollect/api/Factory.scala
@@ -58,7 +58,7 @@ object Factory {
   }
 
   object Worker {
-    lazy val Default: WorkerFactory = AsynchronousDirectDelivery
+    lazy val Default: WorkerFactory = Asynchronous
     lazy val Synchronous: WorkerFactory = new SynchronousWorker(_, Queue.Default, _)
     lazy val SynchronousDirectDelivery: WorkerFactory = new DirectDeliverySynchronousWorker(_, Queue.Default, _)
     lazy val Asynchronous: WorkerFactory = new AsynchronousWorker(_, Queue.Default, _)
