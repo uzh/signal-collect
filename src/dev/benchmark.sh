797f3748c93aee7dc8b7e9c63a5861ed40315ac9
lorenzfischer <lorenz.fischer@gmail.com>
2011-05-24 14:30:16 +0000

    script works :-)

--- src/dev/benchmark.sh
+++ src/dev/benchmark.sh
@@ -4,17 +4,21 @@ working_dir=`mktemp -d`
 cd $working_dir
 
 # checkout Signal/Collect
-svn checkout http://signal-collect.googlecode.com/svn/trunk/core core
+svn checkout --quiet http://signal-collect.googlecode.com/svn/trunk/core core
 
 # install maven
-wget http://mirror.switch.ch/mirror/apache/dist//maven/binaries/apache-maven-2.2.1-bin.tar.gz
-tar xzfv apache-maven-2.2.1-bin.tar.gz
+wget --quiet http://mirror.switch.ch/mirror/apache/dist//maven/binaries/apache-maven-2.2.1-bin.tar.gz
+tar xzf apache-maven-2.2.1-bin.tar.gz
 export PATH=$working_dir/apache-maven-2.2.1/bin:$PATH
 
 # build Signal/Collect
 cd core
 MAVEN_OPTS="-Xms256m -Xmx2048m"
-mvn clean package
+JAVA_HOME=/usr/lib/jvm/java-6-sun
+mvn --quiet -Dmaven.test.skip=true clean package
+
+# run benchmark
+java -Xmx50000m -Xms50000m -cp target/core-0.0.1-SNAPSHOT-jar-with-dependencies.jar signalcollect.benchmark.Benchmark
 
 # remove temporary directory
 cd ~
