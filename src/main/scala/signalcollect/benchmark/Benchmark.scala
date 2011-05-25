84e43073083eb517ecd7be2b9b50c8d0d8c2a6e1
pstutz <philip.stutz@gmail.com>
2011-05-25 11:44:20 +0000

    more minor changes to scalability benchmark

--- src/main/scala/signalcollect/benchmark/Benchmark.scala
+++ src/main/scala/signalcollect/benchmark/Benchmark.scala
@@ -1,4 +1,25 @@
+/*
+ *  @author Lorenz Fischer
+ *  @author Philip Stutz
+ *  
+ *  Copyright 2011 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
 package signalcollect.benchmark
+
 import scala.util.Random
 import signalcollect.interfaces.ComputeGraph
 import signalcollect.api.DefaultBuilder
@@ -49,7 +70,7 @@ object Benchmark extends App {
   var stats = evalGraph.execute
   var computationTime = stats.computationTimeInMilliseconds
   var performanceScore = 100.0 * computationTime.get / 91424.0
-  println("Performance Score: " + performanceScore.toInt + "%")
+  println("Performance Score:\t" + performanceScore.toInt + "%")
   evalGraph.shutDown
 
   evalGraph = buildPageRankGraph(DefaultBuilder.withNumberOfWorkers(3).build, et)
@@ -62,8 +83,8 @@ object Benchmark extends App {
   System.gc
   stats = evalGraph.execute
   val computationTime6Workers = stats.computationTimeInMilliseconds
-  val scalabilityScore = ((computationTime3Workers.get / computationTime6Workers.get) - 1.0) * 100.0
-  println("Scalability Score: " + scalabilityScore.toInt)
+  val scalabilityScore = ((computationTime3Workers.get / computationTime6Workers.get) * 100.0) - 100.0
+  println("Scalability Score:\t" + scalabilityScore.toInt + "%")
   evalGraph.shutDown
     
 }
