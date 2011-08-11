e3e373ea657c53777169ba67e4c4d08382293222
pstutz <philip.stutz@gmail.com>
2011-08-11 14:02:11 +0000

    Disabled debug-level logging for PageRank.

--- src/main/scala/com/signalcollect/examples/PageRank.scala
+++ src/main/scala/com/signalcollect/examples/PageRank.scala
@@ -70,7 +70,7 @@ class Page(id: Any, dampingFactor: Double = 0.85) extends SignalMapVertex(id, 1 
 
 /** Builds a PageRank compute graph and executes the computation */
 object PageRank extends App {
-  val cg = DefaultComputeGraphBuilder.withLoggingLevel(0).build
+  val cg = DefaultComputeGraphBuilder.build
   cg.addVertex(new Page(1))
   cg.addVertex(new Page(2))
   cg.addVertex(new Page(3))
