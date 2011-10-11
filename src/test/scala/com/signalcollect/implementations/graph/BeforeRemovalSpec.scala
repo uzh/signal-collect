eaeaa392081ddcf80ddd8c9eaeaa64aedf44d5ca
pstutz <philip.stutz@gmail.com>
2011-10-11 13:25:05 +0000

    - Reduced time limit on jobs so they get routed to the fast queue (<10h time limit) - Adjusted evaluation to renamed PageRank vertex/edge in core - Adjusted to new beforeRemoval/addIncomingEdge in Vertex trait - Added test case for beforeRemoval function to core

---
+++ src/test/scala/com/signalcollect/implementations/graph/BeforeRemovalSpec.scala
@@ -0,0 +1,55 @@
+/*
+ *  @author Philip Stutz
+ *  
+ *  Copyright 2011 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package com.signalcollect.implementations.graph
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+import org.specs2.matcher.Matcher
+import org.specs2.mock.Mockito
+import com.signalcollect.interfaces._
+import java.util.Map.Entry
+import com.signalcollect.Vertex
+import com.signalcollect.GraphBuilder
+import com.signalcollect.DataGraphVertex
+
+@RunWith(classOf[JUnitRunner])
+class BeforeRemovalSpec extends SpecificationWithJUnit with Mockito {
+
+  "Framework" should {
+    val g = GraphBuilder.build
+    g.addVertex(new BeforeRemovalVertex)
+    g.removeVertex(1)
+
+    "call the onRemoval function of a vertex before removing it" in {
+      RemovalDetector.onRemovalWorked must_== true
+    }
+  }
+
+}
+
+object RemovalDetector {
+  var onRemovalWorked = false
+}
+
+class BeforeRemovalVertex extends DataGraphVertex(1, 0) {
+  def collect(mostRecentSignals: Iterable[Signal]): Int = 0
+  override def beforeRemoval(mb: MessageBus[Any]) = RemovalDetector.onRemovalWorked = true
+}
