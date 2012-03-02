006960ec5a7269bebf9f5d6f39667307e5a53f24
Daniel Strebel <daniel.strebel@gmail.com>
2012-03-02 13:33:16 +0100

    Debug statement for global convergence.

--- src/test/scala/com/signalcollect/features/ComputationTerminationSpec.scala
+++ src/test/scala/com/signalcollect/features/ComputationTerminationSpec.scala
@@ -121,13 +121,14 @@ class ComputationTerminationSpec extends SpecificationWithJUnit with Mockito {
       if (aggregate <= 200.0) {
         println("Computation ended before global condition was met.")
       }
-      if (aggregate > 999.999999) {
+      if (aggregate > 4999.999999) {
         println("Computation converged completely instead of ending when the global constraint was met.")
       }
       if (info.executionStatistics.terminationReason != TerminationReason.GlobalConstraintMet) {
         println("Computation ended for the wrong reason: " + info.executionStatistics.terminationReason)
       }
-      aggregate > 200.0 && aggregate < 999.999999 && info.executionStatistics.terminationReason == TerminationReason.GlobalConstraintMet
+      println("Aggregation async global condition: (200.0 < x < 4999.99) " + aggregate)
+      aggregate > 200.0 && aggregate < 4999.999999 && info.executionStatistics.terminationReason == TerminationReason.GlobalConstraintMet
     }
   }
 
