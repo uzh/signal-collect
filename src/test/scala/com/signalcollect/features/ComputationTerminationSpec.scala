fd110ae99a3623fd24b6d269970605c434f46d03
Philip Stutz <philip.stutz@gmail.com>
2012-03-05 10:33:53 +0100

    Merged changes to Global Termination Condition test

--- src/test/scala/com/signalcollect/features/ComputationTerminationSpec.scala
+++ src/test/scala/com/signalcollect/features/ComputationTerminationSpec.scala
@@ -121,13 +121,14 @@ class ComputationTerminationSpec extends SpecificationWithJUnit with Mockito {
       if (aggregate <= 200.0) {
         println("Computation ended before global condition was met.")
       }
-      if (aggregate > 999) {
+      if (aggregate > 999.0) {
         println("Computation converged completely instead of ending when the global constraint was met.")
       }
       if (info.executionStatistics.terminationReason != TerminationReason.GlobalConstraintMet) {
         println("Computation ended for the wrong reason: " + info.executionStatistics.terminationReason)
       }
-      aggregate > 200.0 && aggregate < 999 && info.executionStatistics.terminationReason == TerminationReason.GlobalConstraintMet
+      println("Aggregation async global condition: (200.0 < shouldBe < 999.0) " + aggregate)
+      aggregate > 200.0 && aggregate < 999.0 && info.executionStatistics.terminationReason == TerminationReason.GlobalConstraintMet
     }
   }
 
