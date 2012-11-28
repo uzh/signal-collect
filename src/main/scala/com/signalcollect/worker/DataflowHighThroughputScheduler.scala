74034c3283a1873015df9ac8ab40728d7eb59e2b
Philip Stutz <philip.stutz@gmail.com>
2012-11-28 12:56:43 +0100

    Put the high throughput scheduler into a separate trait.

---
+++ src/main/scala/com/signalcollect/worker/DataflowHighThroughputScheduler.scala
@@ -0,0 +1,37 @@
+/*
+ *  @author Philip Stutz 
+ *  
+ *  Copyright 2012 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package com.signalcollect.worker
+
+trait DataflowHighThroughputScheduler[Id, Signal] extends AkkaWorker[Id, Signal] {
+  override def scheduleOperations {
+    if (!vertexStore.toCollect.isEmpty) {
+      vertexStore.toCollect.process(executeCollectOperationOfVertex(_))
+    }
+    if (!vertexStore.toSignal.isEmpty && !messageQueue.hasMessages && maySignal) {
+      vertexStore.toSignal.process(executeSignalOperationOfVertex(_), Some(batchProcessSize))
+      messageBus.flush
+      continueSignalingReceived = false
+    }
+    if (!continueSignalingReceived && !awaitingContinueSignaling && !vertexStore.toSignal.isEmpty) {
+      messageBus.sendToActor(self, ContinueSignaling)
+      awaitingContinueSignaling = true
+    }
+  }
+}
