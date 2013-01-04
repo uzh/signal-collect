a070f5f3ccb9c0293b9a8dea49fbfaadcd91cbd2
Daniel Strebel <daniel.strebel@gmail.com>
2013-01-04 13:28:57 +0100

    println to monitor progress in local jobsubmission

--- src/main/scala/com/signalcollect/nodeprovisioning/torque/LocalJobSubmitter.scala
+++ src/main/scala/com/signalcollect/nodeprovisioning/torque/LocalJobSubmitter.scala
@@ -5,6 +5,7 @@ import scala.sys.process._
 class LocalJobSubmitter(mailAddress:String = "") extends AbstractJobSubmitter(mailAddress) {
 
   def executeCommandOnClusterManager(command: String): String = {
+    println(command)
     command !!
   }
 }
