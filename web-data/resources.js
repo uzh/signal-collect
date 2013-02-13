7f82ffb5be7460e9d33ad670ff37c4f1773d52b9
Silvan Troxler <silvan.troxler@gmail.com>
2013-02-13 16:30:05 +0100

    hide graph2, fix graph2 and improve styling, try to add y-axis

--- web-data/resources.js
+++ web-data/resources.js
@@ -4,29 +4,44 @@ scc.modules.resources = function() {
   var graph1, graph2, reloadTimeout
 
   this.onopen = function () {
-    graph1 = new Rickshaw.Graph( {
-      element: document.querySelector("#res_graph1"),
-      width: 400,
-      height: 250,
-      renderer: 'line',
-      series: new Rickshaw.Series([{ name: 'This' }])
-    });
-    graph1.render();
+//    graph1 = new Rickshaw.Graph( {
+//      element: document.querySelector("#res_graph1"),
+//      width: 400,
+//      height: 250,
+//      renderer: 'line',
+//      series: new Rickshaw.Series([{ name: 'This' }])
+//    });
+//    graph1.render();
 
     var tv = 1000;
     graph2 = new Rickshaw.Graph( {
       element: document.getElementById("res_graph2"),
-      width: 450,
-      height: 250,
-      renderer: 'line',
-      series: new Rickshaw.Series.FixedDuration([{ name: 'one' }], undefined, {
+      width: 540,
+      height: 240,
+//      renderer: 'line',
+      series: new Rickshaw.Series.FixedDuration([{ name: 'one', color: 'steelblue' }], undefined, {
         timeInterval: tv,
-        maxDataPoints: 100,
-        timeBase: new Date().getTime() / 1000
+        maxDataPoints: 50,
+//        timeBase: new Date().getTime() / 1000,
+        color: 'steelblue'
       }) 
+    });
+    
+    var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+    	graph: graph2
     } );
     
-    graph2.render();
+//    var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
+    
+//    var y_axis = new Rickshaw.Graph.Axis.Y( {
+//        graph: graph2,
+//        orientation: 'left',
+//        tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+//        element: document.getElementById('y_axis'),
+//    });
+//    y_axis.render();
+    
+//    graph2.render();
     scc.webSocket.send("resources")
   }
     
@@ -37,18 +52,19 @@ scc.modules.resources = function() {
 
   this.onmessage = function(j) {
     // graph 1
-    var newData  = [];
-    for (var index in j.workerStatistics["collectOperationsExecuted"]) {
-      newData.push({ 'x': index, 
-                     'y': j.workerStatistics["messagesSent"][index]});
-    }
-    graph1.series[0].data = newData;
-    graph1.update();
+//    var newData  = [];
+//    for (var index in j.workerStatistics["collectOperationsExecuted"]) {
+//      newData.push({ 'x': index, 
+//                     'y': j.workerStatistics["messagesSent"][index]});
+//    }
+//    graph1.series[0].data = newData;
+//    graph1.update();
 
     // graph 2
-    newData = { one: j.workerStatistics["messagesSent"] };
+	
+    newData = { one: j.workerStatistics["messagesSent"][0] - Math.floor(Math.random()*(j.workerStatistics["messagesSent"][0]/5)) };
     graph2.series.addData(newData);
-    //graph2.render();
+    graph2.render();
     
     reloadTimeout = setTimeout(function(){
       scc.webSocket.send("resources")
