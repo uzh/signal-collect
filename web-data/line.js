a521b7919b3feee45ca6a7aa9d4b74d5f9793f74
st <silvan.troxler@gmail.com>
2013-03-03 16:50:43 +0100

    add updates to standalone chart

--- web-data/line.js
+++ web-data/line.js
@@ -1,28 +1,25 @@
 /*
  * TODO
- * - add tool tips
- * - add auto update (with animation)
- * - add lines for setting up, graph building, calculation
+ * - add update animation
  */
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 900 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
+var now = new Date;
+var interval = 2000;
+
 var parseDate = d3.time.format("%d-%b-%y").parse;
 var data = [
             [
-             {date:parseDate("1-May-12"), value:500, id:"average"},
-             {date:parseDate("3-May-12"), value:450, id:"average"},
-             {date:parseDate("5-May-12"), value:600, id:"average"}
+             {date:now, value:500, id:"average"}
             ],
             [
-              {date:parseDate("2-May-12"), value:400, id:"Node1"},
-              {date:parseDate("3-May-12"), value:200, id:"Node1"}
+              {date:now, value:400, id:"Node1"}
             ],
             [
-             {date:parseDate("1-May-12"), value:400, id:"Node1"},
-             {date:parseDate("5-May-12"), value:200, id:"Node1"}
+             {date:now, value:400, id:"Node1"}
             ]
            ];
 
@@ -34,9 +31,10 @@ var y = d3.scale.linear()
     .range([height, 0]);
 
 // add scale of the axes
-x.domain(d3.extent(data[0], function(d) { return d.date; }));
-x.domain([parseDate("30-Apr-12"), parseDate("6-May-12")]);
-y.domain([0, d3.max(data.map(function(d) { return d3.max(d, function(dm) { return dm.value; }); } )) * 1.1]);
+//x.domain(d3.extent(data[0], function(d) { return d.date; }));
+//x.domain([nowTimestamp-60, nowTimestamp+60]);
+x.domain([new Date(+(now)-(10*1000)), new Date(+(now)+(120*1000))]);
+y.domain([0, 1]);
 
 var xAxis = d3.svg.axis()
     .scale(x)
@@ -103,7 +101,7 @@ aLineContainer.selectAll(".dot")
   .append("circle")
   .attr("class", "dot")
   .attr("r", 3.5)
-  .on("mouseover", function(d) {      
+  .on("mouseover", function(d) {
             div.transition()        
                .duration(100)      
                .style("opacity", .9);      
@@ -117,12 +115,76 @@ aLineContainer.selectAll(".dot")
                 .style("opacity", 0);   
         });
 
+var currentDate = now;
+function update() {
+  console.log("updating");
+  
+  currentDate = new Date(+(currentDate)+interval);
+  
+  var newData = [
+                 {date:currentDate, value:(Math.random()+1)*200, id:"average"},
+                 {date:currentDate, value:(Math.random()+1)*100, id:"min"},
+                 {date:currentDate, value:(Math.random()+2)*200, id:"max"},
+                ];
+  
+  var lowestXDomain      = x.domain()[0];
+  var highestXDomain     = x.domain()[1];
+  var currentHighestDate = d3.max(data[0], function(d) { return d.date });
+  
+  // is current highest date currently being showed?
+  if (lowestXDomain <= currentHighestDate && currentHighestDate <= highestXDomain) {
+    var newHighestDate = d3.max(newData, function(d) { return d.date });
+    // if new highest date is out of the domain, update the domain
+    if (highestXDomain < newHighestDate) {
+      x.domain([new Date(+(lowestXDomain)+(interval)), newHighestDate]);
+      console.log("update x domain");
+//      svg.select("g.y.axis").transition().duration(300).ease("linear").call(yAxis);
+    }
+  }
+  
+  newData.forEach(function(d, i) {
+    data[i].push(d);
+  });
+//  console.dir(data);
+  
+  aLineContainer
+  .attr("d", line)
+  .attr("transform", null)
+//  .transition()
+//  .duration(500)
+//  .ease("linear")
+//  .attr("transform", "translate(" + x(0) + ")")
+//  .each("end", update);
+  
+//  d3.selectAll("path.line").data({date:parseDate("4-May-12"), value:Math.random()*500, id:"Node1"}).enter();
+
+  // update domains
+//  x.domain([parseDate("30-Apr-12"), parseDate("6-Jun-12")]);
+  y.domain([0, d3.max(data.map(function(d) { return d3.max(d, function(dm) { return dm.value; }); } )) * 1.1]);
+//  xAxis.scale(x);
+//  yAxis.scale(y);
+  
+
+  
+//  lines = svgBox.selectAll("g").data([newData]);
+//  lines.enter().append("g");
+  
+  draw();
+}
+
+window.setInterval(function() {
+  update();
+}, interval);
+
+
 draw();
 
 function draw() {
   svg.select("g.x.axis").call(xAxis);
-  svg.select("g.y.axis").call(yAxis);
-//  svg.select("path.area").attr("d", area);
+  svg.select("g.y.axis").transition().duration(300).ease("linear").call(yAxis);
+
+  
+  //  svg.select("path.area").attr("d", area);
   svg.selectAll("path.line").attr("d", line);
   aLineContainer.selectAll("circle.dot").attr("cx", line.x()).attr("cy", line.y());
 //  d3.select("#footer span").text("U.S. Commercial Flights, " + x.domain().map(format).join("-"));
