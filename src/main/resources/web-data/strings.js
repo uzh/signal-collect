71842f643270abfa7ae61ec0df1595988759d62e
Silvan Troxler <silvan.troxler@gmail.com>
2013-04-16 12:43:20 +0200

    add strings.js

---
+++ src/main/resources/web-data/strings.js
@@ -0,0 +1,6 @@
+STR = {"searchByID": "Search and hit Enter to execute",
+       "pickNode": "Enter ID or select using mouse",
+       "enterState": "Enter state",
+       "noConditions": "No conditions specified",
+       "noExecution": "The interactive execution mode is unavailable, retrying..."
+}
