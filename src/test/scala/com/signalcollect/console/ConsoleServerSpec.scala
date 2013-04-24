401e37f77bfe2d334a76edf1dcd7f97b91d69050
Silvan Troxler <silvan.troxler@gmail.com>
2013-04-24 14:59:48 +0200

    add first, not-yet-functional Scala test, make some cleanups

---
+++ src/test/scala/com/signalcollect/console/ConsoleServerSpec.scala
@@ -0,0 +1,67 @@
+/*
+ *  @author Silvan Troxler
+ *  
+ *  Copyright 2013 University of Zurich
+ *      
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ */
+
+package com.signalcollect.console
+
+import org.junit.runner.RunWith
+import org.specs2.mock.Mockito
+import org.specs2.mutable.SpecificationWithJUnit
+import com.signalcollect.GraphBuilder
+import org.specs2.runner.JUnitRunner
+import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.io.IOException
+import java.net.ServerSocket
+
+@RunWith(classOf[JUnitRunner])
+class ConsoleServerSpec extends SpecificationWithJUnit with Mockito {
+
+  sequential
+
+  "ConsoleServer" should {
+
+    "start successfully" in {
+      val port = 8098
+      val socketAddress = new InetSocketAddress(InetAddress.getByName(null), port)
+      val timeoutInMilliseconds = 5000
+      val socket = new Socket
+      var online = false
+
+      val graph = GraphBuilder.withConsole(true, port).build
+      
+      try {
+        socket.connect(socketAddress, timeoutInMilliseconds)
+        online = true
+      } catch {
+        case _ : Throwable => online = false
+      } finally {
+        try {
+            socket.close();
+        } catch { case _ : Throwable => }
+      }
+
+      graph.shutdown
+      
+      online === true
+    }
+
+  }
+
+}
