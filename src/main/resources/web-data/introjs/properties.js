dc06faf5df9b496107cf481d83695e98359ddf79
cshapeshifter <shapeshifter@archlinux.us>
2013-05-02 22:41:29 +0200

    More detailed tour for graph UI

--- src/main/resources/web-data/introjs/properties.js
+++ src/main/resources/web-data/introjs/properties.js
@@ -10,27 +10,59 @@ $(document).ready(function() {
     // we do not have to show the introduction
     return false;
   }
+  var url = document.URL.split("/");
+  var loc = url[0] + "//" + url[2]
+  var locGraph = '<a href="' + loc + '/graph">' + loc + '/graph</a>';
+  var locResources = '<a href="' + loc + '/resources">' + loc + '/resources</a>';
   
   // define the introduction selectors and texts
   var properties = {
     "#title":
-      "<strong>Welcome! This is a guided tour through the user interface of the Console Server for Signal/Collect." + 
-      "</strong><br />You can easily navigate through these steps by using the arrow keys or your mouse.",
+      "<strong>Welcome! This is a guided tour through the user interface of the Signal/Collect Console.</strong><br/>" + 
+      "Navigate these steps by using the arrow keys or your mouse and hit ESC or click 'Skip' to cancel the tour. " +
+      "You can revisit the tour at any time by clicking on <strong>Tour</strong> in the lower right corner",
     "#modes":
-      "The Console Server consists of two main views: The 'Graph View' and the 'Resource View'.",
-    "#cNodeSelection":
-      "In the Graph view, you can navigate the graph on which the current computation is running. In order to" +
-      " display only certain vertices of the graph, you can choose them in this section by either showing 'top" +
-      " vertices' or a specific vertice by ID.",
+      "The Console Server consists of two main views: The <strong>Graph View</strong> and the <strong>Resource View</strong>. " +
+      "You can switch between the views using these buttons, or you may access a single view under " + locGraph + 
+      " or " + locResources + " without loading the other.",
+    "#cVertexSelection":
+      "In the Graph view, you can navigate the graph of your current computation is running. You start out with an empty " +
+      "canvas, onto which you add the vertices you're interested in. When the view becomes cluttered, you can remove vertices you don't " +
+      "need anymore.",
+    "#gp_container":
+      "The choices you make here may have an impact on how quickly your query completes and on how smooth the graph will be " +
+      "displayed. <br/>" +
+      "<ul><li>The <strong>hop radius</strong> determines the maximum distance travelled from the source vertex when loading its vicinity</li>" +
+      "    <li><strong>computing incoming edges</strong> is much more expensive than only computing outgoing ones when loading vicinities</li>" +
+      "    <li>The <strong>refresh rate</strong> determines how often the graph auto-reloads when running a computation continuously</li></ul>",
+    "#gs_container":
+      "Add vertices by exceptional properties or by searching for vertices using a string. If any vertex has an id which contains the string, " +
+      "it will be added to the canvas. Load the vicinity of a vertex by <strong>double-clicking</strong> it, or expand several vicinities at once by " +
+      "drawing a rectangle around them.",
+    "#gd_container":
+      "Clear the entire canvas or remove nodes by drawing a rectangle around them. You can also remove any vertices that don't have any edges.",
     "#cGraphDesign":
-      "In this section, you can choose how the vertices in the graph should look like. You can define node size," +
-      " color and outline color based on certain node properties. You can also define the node vicinity and other" +
-      " characteristics.",
+      "Choose how the vertices in the graph should look like. You can make the vertex size, fill color and outline color correspond to " +
+      "certain properties of the vertex.",
     "#cGraphControl":
-      "In this section you can add different breakpoints to stop the computation when certain events come in.",
+      "Select a vertex and add break conditions to halt the computation when certain events occur.",
     "#controls":
-      "When the Console is used, the computation usually does not start automatically. Instead the user can step" +
-      " through the computation, run it until it is paused again, or terminate completely.",
+      "If your graph computation is using the <strong>interactive execution mode</strong>, then you can use these buttons to control it. ",
+    "#reset":
+      "<strong>Reset</strong> the computation to restore all vertices to their initial state",
+    "#pause":
+      " <strong>Pause</strong> the computation if it is running",
+    "#step":
+      " Performing a <strong>partial step</strong> will walk you through all the states of a single iteration, pausing before each of them: " +
+               "1st condition check, signalling, 2nd condition check, collecting and global termination check.",
+    "#collect":
+      " Performing a <strong>full step</strong> continues the iteration and stops before the next 1st condition check.",
+    "#continue":
+      " Clicking <strong>continue</strong> will only pause the computation when you hit pause, or if a break condition fires.",
+    "#terminate":
+      " Click <strong>terminate</strong> to end the computation and quit Signal/Collect.",
+    "#stateContainer":
+      " The current state and the current iteration are displayed here",
     "#mode_resources":
       "To change to the 'Resources', just click on it.",
     "#crs_statistics h1":
@@ -55,6 +87,7 @@ $(document).ready(function() {
   $.each(properties, function(selector, val) {
     $(selector).attr("data-step", i++).attr("data-intro", val);
   });
+  $("#reset,#pause,#step,#collect,#continue,#terminate,#stateContainer").attr("data-position", "top");
 
   // show the introduction
   var intro = introJs();
