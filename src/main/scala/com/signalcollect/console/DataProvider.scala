42723adbd7c82e14cb5cb09ae2cca8b3a74a188b
cshapeshifter <shapeshifter@archlinux.us>
2013-05-07 17:36:41 +0200

    Document DataProvider.scala

--- src/main/scala/com/signalcollect/console/DataProvider.scala
+++ src/main/scala/com/signalcollect/console/DataProvider.scala
@@ -39,6 +39,7 @@ import akka.event.Logging.LogLevel
 import akka.event.Logging.LogEvent
 import akka.actor.ActorLogging
 
+/** The trait defining the interface every DataProvider has to implement. */
 trait DataProvider {
   def fetch(): JObject
   def fetchInvalid(msg: JValue = JString(""), 
@@ -47,6 +48,11 @@ trait DataProvider {
   }
 }
 
+/** DataProvider that wraps a stack trace.
+  *
+  * @constructor create a new ErrorDataProvider
+  * @param e the exception that has been thrown
+  */
 class ErrorDataProvider(e: Exception) extends DataProvider {
   def fetchStacktrace(): String = {
     val sw = new StringWriter()
@@ -60,6 +66,16 @@ class ErrorDataProvider(e: Exception) extends DataProvider {
   }
 }
 
+/** DataProvider used when invalid communication occurs.
+  *
+  * When an invalid request is received, this provider is used to supply the
+  * original message that caused the problem plus a comment that could further
+  * explain the problem.
+  *
+  * @constructor create a new InvalidDataProvider
+  * @param msg the original request that caused the problem
+  * @param comment more information on why the request is invalid
+  */
 class InvalidDataProvider(msg: JValue, comment: String = "No comment") extends DataProvider {
   def fetch(): JObject = {
     ("provider" -> "invalid") ~
@@ -68,6 +84,15 @@ class InvalidDataProvider(msg: JValue, comment: String = "No comment") extends D
   }
 }
 
+/** DataProvider used when another data provider can't satisfy the request
+  *
+  * It can occur that a valid request cannot be satisfied, for example because
+  * the underlying data is not available yet. In that case, this data provider
+  * is used to inform the client of the problem
+  *
+  * @constructor create a new NotReadyDataProvider
+  * @param msg the original request
+  */
 class NotReadyDataProvider(msg: String) extends DataProvider {
   implicit val formats = DefaultFormats
   val j = parse(msg)
@@ -80,8 +105,13 @@ class NotReadyDataProvider(msg: String) extends DataProvider {
   }
 }
 
+/** DataProvider used to fetch the current state of the computation.
+  *
+  * @constructor create a new StateDataProvider
+  * @param socket the WebSocketConsoleServer 
+  */
 class StateDataProvider[Id](socket: WebSocketConsoleServer[Id])
-                             extends DataProvider {
+                            extends DataProvider {
   def fetch(): JObject = {
     val reply: JObject = socket.execution match {
       case Some(e) => 
@@ -94,10 +124,20 @@ class StateDataProvider[Id](socket: WebSocketConsoleServer[Id])
   }
 }
 
-
+/** DataProvider which serves execution, graph and system configurations.
+  *
+  * The execution configuration is taken from the WebSocketConsoleServer which
+  * owns the execution mode object. The graph configuration is taken from the
+  * coordinator. Finally, the system information is taken from the Java
+  * System.getProperties collection.
+  *
+  * @constructor create a new ConfigurationDataProvider
+  * @param socket the WebSocketConsoleServer (who knows the exec conf)
+  * @param coordinator the Coordinator (who knows the graph conf)
+  */
 class ConfigurationDataProvider[Id](socket: WebSocketConsoleServer[Id],
-                              coordinator: Coordinator[Id, _],
-                              msg: JValue) extends DataProvider {
+                                    coordinator: Coordinator[Id, _]) 
+                                    extends DataProvider {
   def fetch(): JObject = {
     val executionConfiguration = socket.executionConfiguration match {
       case Some(e: ExecutionConfiguration) => Toolkit.unpackObject(e)
@@ -106,10 +146,15 @@ class ConfigurationDataProvider[Id](socket: WebSocketConsoleServer[Id],
     ("provider" -> "configuration") ~ 
     ("executionConfiguration" -> executionConfiguration) ~
     ("graphConfiguration" -> Toolkit.unpackObjects(Array(socket.graphConfiguration))) ~
-    ("systemProperties" -> propertiesAsScalaMap(System.getProperties()))
+    ("systemProperties" -> propertiesAsScalaMap(System.getProperties))
   }
 }
 
+/** DataProvider which serves akka log messages from the coordinator
+  *
+  * @constructor create a new LogDataProvider
+  * @param coordinator the Coordinator
+  */
 class LogDataProvider[Id](coordinator: Coordinator[Id, _]) extends DataProvider {
   def fetch(): JObject = {
     ("provider" -> "log") ~ 
@@ -117,12 +162,19 @@ class LogDataProvider[Id](coordinator: Coordinator[Id, _]) extends DataProvider 
   }
 }
 
+/** Data structure used to model client control requests */
 case class ControlsRequest(
   control: Option[String]
 )
 
+/** Provider that accepts execution commands.
+  *
+  * @constructor create a new ControlsProvider
+  * @param socket the WebSocketConsoleServer
+  * @param msg the request by the client
+  */
 class ControlsProvider(socket: WebSocketConsoleServer[_],
-                           msg: JValue) extends DataProvider {
+                       msg: JValue) extends DataProvider {
 
   implicit val formats = DefaultFormats
   var execution: Option[Execution] = socket.execution
@@ -157,18 +209,38 @@ class ControlsProvider(socket: WebSocketConsoleServer[_],
   }
 }
 
+/** Data structure used to model client break condition requests */
 case class BreakConditionsRequest(
   action: Option[String],
   name: Option[String],
   id: Option[String],
   props: Option[Map[String,String]]
 )
+
+/** Data structure used to model replies to the client */
 case class BreakConditionContainer(
   id: String,
   name: String,
   props: Map[String,String]
 )
 
+/** Provider that can be used to add, remove and check on break conditions.
+  *
+  * The break conditions are stored with the interactive execution mode. Each
+  * condition has a unique ID which it receives when it's being created. During
+  * the computation, various breakconditions will be checked at different
+  * times.
+  *
+  * Wether the client supplies an action (to add or remove a condition) or not,
+  * the provider will always answer with a list of configured conditions and a 
+  * list of reached conditions. If the action could not be performed (e.g. if
+  * an invalid vertex ID has been supplied) an error message will be included.
+  *
+  * @constructor create a new BreakConditionsProvider
+  * @param coordinator the Coordinator
+  * @param socket the WebSocketConsoleServer
+  * @param msg the request by the client
+  */
 class BreakConditionsProvider[Id](coordinator: Coordinator[Id, _], 
                                   socket: WebSocketConsoleServer[Id],
                                   msg: JValue) extends DataProvider {
@@ -203,6 +275,9 @@ class BreakConditionsProvider[Id](coordinator: Coordinator[Id, _],
                       socket.executionConfiguration match {
                         case Some(c) =>
                           try {
+                            // Create the condition. The input is validated
+                            // inside the constructor of BreakCondition and
+                            // when a requirement fails, an exception is thrown
                             val condition = new BreakCondition(socket.graphConfiguration, 
                                                                c, n, props, workerApi)
                             e.addCondition(condition)
@@ -238,6 +313,7 @@ class BreakConditionsProvider[Id](coordinator: Coordinator[Id, _],
   }
 }
 
+/** Data structure used to model client graph data requests */
 case class GraphDataRequest(
   vertexIds: Option[List[String]],
   vicinityRadius: Option[Int],
@@ -250,6 +326,12 @@ case class GraphDataRequest(
   collectThreshold: Option[Double]
 )
 
+/** Provider that can be used to query the graph.
+  *
+  * @constructor create a new GraphDataProvider
+  * @param coordinator the Coordinator
+  * @param msg the request by the client
+  */
 class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue) 
                             extends DataProvider {
 
@@ -263,6 +345,22 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
   var signalThreshold = 0.01
   var collectThreshold = 0.0
 
+  /** Recursively load the vicinity of a vertex.
+    *
+    * The vicinity of a vertex is the set of vertices that can be reached by
+    * traveling a maximum of {{radius}} times along the edges leading away
+    * from the vertex. A {{radius}} of 1 denotes only the vertices that share
+    * an edge with the node. Because a vertex already knows its outgoing edges,
+    * it is cheap to find the outgoing vicinity. However, for the incoming
+    * vicinity, an aggregation has to be performed to find the vertices that 
+    * target one of the vertices we're interested in, hence it's an expensive
+    * operation.
+    *
+    * @param sourceIds set of vertex Ids to use in the search
+    * @param radius how far to travel from the source vertices
+    * @param incoming also consider incoming edges (costs {{radius}} aggregations)
+    * @return the original set plus the set of vertex Ids in the vicinity
+    */
   def findVicinity(sourceIds: Set[Id], radius: Int = 3, 
                    incoming: Boolean = false): Set[Id] = {
     if (radius == 0) { sourceIds }
@@ -282,6 +380,19 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
     }
   }
 
+  /** Fetch a JObject representation of the vertices and edges of the graph.
+    * 
+    * This function will load the vertices as supplied by the {{vertexIds}} 
+    * set but also any vertices in the object-scope variable 
+    * {{vertexIdStrings}}. The client will usually request a specific part of
+    * the graph, but also supply a list of vertices that should be loaded.
+    *
+    * All the other fetch functions make use of this function to finalize
+    * the request.
+    *
+    * @param vertexIds set of vertex Ids to load
+    * @return the JObject containing the requested vertices and their edges
+    */
   def fetchGraph(vertexIds: Set[Id] = Set[Id]()): JObject = {
     val vertices = workerApi.aggregateAll(
                      new FindVerticesByIdsAggregator[Id](vertexIdStrings))
@@ -294,6 +405,7 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
     graph
   }
 
+  /** Fetch vertices ordered by their highest or lowest state. */
   def fetchByTopState(inverted: Boolean = false): JObject = {
     val topState = workerApi.aggregateAll(
                      new TopStateAggregator[Id](targetCount, inverted)).take(targetCount)
@@ -301,6 +413,7 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
     fetchGraph(vertexIds)
   }
 
+  /** Fetch vertices ordered by their degree. */
   def fetchByTopDegree(): JObject = {
     val vertexIds = workerApi.aggregateAll(new TopDegreeAggregator[Id](targetCount))
                              .toSeq.sortBy(-_._2)
@@ -310,6 +423,7 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
     fetchGraph(vertexIds)
   }
 
+  /** Fetch vertices with a {{scoreType}} score above the threshold. */
   def fetchByAboveThreshold(scoreType: String): JObject = {
     val threshold = if (scoreType == "signal") signalThreshold else collectThreshold
     val aboveThreshold = workerApi.aggregateAll(
@@ -318,12 +432,14 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
     fetchGraph(vertexIds)
   }
 
+  /** Fetch vertices whose Ids contain the given substring {{s}}. */
   def fetchBySubstring(s: String): JObject = {
     val vertexIds = workerApi.aggregateAll(
                      new FindVertexIdsBySubstringAggregator[Id](s, targetCount))
     fetchGraph(vertexIds)
   }
 
+  /** Fetch a random sample of vertices. */
   def fetchSample(): JObject = {
     val vertexIds = workerApi.aggregateAll(new SampleAggregator[Id](targetCount))
     fetchGraph(vertexIds)
@@ -331,6 +447,8 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
 
   def fetch(): JObject = {
     val request = (msg).extract[GraphDataRequest]
+
+    // Override default values if any
     request.vertexIds match {
       case Some(ids) => vertexIdStrings = ids.take(1000)
       case otherwise => 
@@ -355,6 +473,8 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
       case Some(t) => collectThreshold = t
       case otherwise => 
     }
+
+    // route request and fetch graph data
     val graphData = request.query match {
       case Some("substring") => request.substring match {
         case Some(s) => fetchBySubstring(s)
@@ -380,6 +500,12 @@ class GraphDataProvider[Id](coordinator: Coordinator[Id, _], msg: JValue)
   }
 }
 
+/** Provider offering resource and other statistics on workers and nodes.
+  *
+  * @constructor create a new ReourcesDataProvider
+  * @param coordinator the Coordinator
+  * @param msg the request by the client
+  */
 class ResourcesDataProvider(coordinator: Coordinator[_, _], msg: JValue)
       extends DataProvider {
 
