413b8ea0d88a6ee773c44b910ae1e4da0218bbdd
Philip Stutz <philip.stutz@gmail.com>
2013-07-30 13:41:05 +0200

    Adapted deflate bufixes from pre-release Kryo.

---
+++ src/main/scala/com/esotericsoftware/kryo/serializers/FixedDeflateSerializer.scala
@@ -0,0 +1,47 @@
+package com.esotericsoftware.kryo.serializers
+
+import java.io.IOException
+import java.util.zip.Deflater
+import java.util.zip.DeflaterOutputStream
+import java.util.zip.Inflater
+import java.util.zip.InflaterInputStream
+
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.KryoException
+import com.esotericsoftware.kryo.Serializer
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.InputChunked
+import com.esotericsoftware.kryo.io.Output
+import com.esotericsoftware.kryo.io.OutputChunked
+
+class FixedDeflateSerializer[G](
+  val serializer: Serializer[G],
+  val compressionLevel: Int = 4,
+  val noHeaders: Boolean = true)
+    extends Serializer[G] {
+
+  def write(kryo: Kryo, output: Output, toSerialize: G) {
+    val deflater = new Deflater(compressionLevel, noHeaders)
+    val outputChunked = new OutputChunked(output, 256)
+    val deflaterStream = new DeflaterOutputStream(outputChunked, deflater)
+    val deflaterOutput = new Output(deflaterStream, 256)
+    kryo.writeObject(deflaterOutput, toSerialize, serializer)
+    deflaterOutput.flush
+    try {
+      deflaterStream.finish
+    } catch {
+      case io: IOException => throw new KryoException(io)
+    }
+    outputChunked.endChunks
+  }
+
+  def read(kryo: Kryo, input: Input, tpe: Class[G]): G = {
+    // The inflater would read from input beyond the compressed bytes if chunked enoding wasn't used.
+    val inflaterStream = new InflaterInputStream(new InputChunked(input, 256), new Inflater(noHeaders))
+    kryo.readObject(new Input(inflaterStream, 256), tpe, serializer)
+  }
+
+  override def copy(kryo: Kryo, original: G): G = {
+    serializer.copy(kryo, original)
+  }
+}
