cafdefd71ad2f0ce29ca69403d229c4936052337
Philip Stutz <philip.stutz@gmail.com>
2013-10-22 12:34:15 +1100

    Added contains and insert operations to compact int set.

--- src/main/scala/com/signalcollect/util/Ints.scala
+++ src/main/scala/com/signalcollect/util/Ints.scala
@@ -11,14 +11,15 @@ object Ints {
   def create(ints: Array[Int]): Array[Byte] = {
     val sorted = ints.sorted
     var i = 0
-    var previous = 0
+    var previous = -1
     while (i < sorted.length) {
       val tmp = sorted(i)
+      // TODO: Investigate encoding of 0, improve if possible.
       sorted(i) = sorted(i) - previous - 1
       previous = tmp
       i += 1
     }
-    val baos = new ByteArrayOutputStream()
+    val baos = new ByteArrayOutputStream
     val dos = new DataOutputStream(baos)
     i = 0
     while (i < sorted.length) {
@@ -65,9 +66,87 @@ object Ints {
   }
 
   implicit class IntSet(encoded: Array[Byte]) extends Traversable[Int] {
+
+    /**
+     * Adds an integer to the set and returns the updated set.
+     *
+     */
+    def add(i: Int): Array[Byte] = {
+
+      Array(1)
+    }
+
+    /**
+     * Returns true iff item is in the set.
+     */
+    def contains(item: Int): Boolean = findIndex(item) >= 0
+
+    /**
+     * Inserts item into the set.
+     */
+    def insert(item: Int): Array[Byte] = {
+      val baos = new ByteArrayOutputStream
+      val dos = new DataOutputStream(baos)
+      var itemPresentOrWritten = false
+      var previous = -1
+      foreach(current => {
+        if (!itemPresentOrWritten) {
+          if (current < item) {
+          } else if (current == item) {
+            itemPresentOrWritten = true
+          } else if (current > item) {
+            writeUnsignedVarInt(item - previous - 1, dos)
+            previous = item
+            itemPresentOrWritten = true
+          }
+        }
+        writeUnsignedVarInt(current - previous - 1, dos)
+        previous = current
+      })
+      if (!itemPresentOrWritten) {
+        writeUnsignedVarInt(item - previous - 1, dos)
+      }
+      dos.flush
+      baos.flush
+      baos.toByteArray
+    }
+
+    /**
+     * Returns the index of the first byte of element
+     * item, iff item is contained in the set.
+     * Returns -1 otherwise.
+     */
+    private def findIndex(item: Int): Int = {
+      val notFound = -1
+      var i = 0
+      var previousInt = -1
+      var startingIndexOfCurrentInt = 0
+      var currentInt = 0
+      var shift = 0
+      while (i < encoded.length) {
+        val readByte = encoded(i)
+        currentInt |= (readByte & leastSignificant7BitsMask) << shift
+        shift += 7
+        if ((readByte & hasAnotherByte) == 0) {
+          // Next byte is no longer part of this Int.
+          previousInt += currentInt + 1
+          if (previousInt == item) {
+            return startingIndexOfCurrentInt
+          } else if (previousInt > item) {
+            return notFound
+          }
+          currentInt = 0
+          shift = 0
+          startingIndexOfCurrentInt = i + 1
+        }
+        i += 1
+      }
+      notFound
+    }
+
     def foreach[U](f: Int => U) {
       var i = 0
-      var previousInt = 0
+      var previousInt = -1
       var currentInt = 0
       var shift = 0
       while (i < encoded.length) {
--- src/test/scala/com/signalcollect/util/IntsSpec.scala
+++ src/test/scala/com/signalcollect/util/IntsSpec.scala
@@ -8,16 +8,34 @@ import com.signalcollect.util.Ints._
 
 class IntsSpec extends FlatSpec with ShouldMatchers with Checkers {
 
-  "IntSet" should "correctly compress sets of Ints" in {
+  "IntSet" should "compress sets of Ints" in {
     check(
       (ints: Array[Int]) => {
         val intSet = ints.toSet
-        println(intSet)
         val compact = Ints.create(ints)
-        val expanded = IntSet(compact).toSet
-        expanded == intSet
+        IntSet(compact).toSet == intSet
       },
-      minSuccessful(200))
+      minSuccessful(1000))
+  }
+
+  it should "support the 'contains' operation" in {
+    check(
+      (ints: Array[Int], item: Int) => {
+        val intSet = ints.toSet
+        val compact = Ints.create(ints)
+        IntSet(compact).contains(item) == intSet.contains(item)
+      },
+      minSuccessful(1000))
+  }
+
+  it should "support the 'insert' operation" in {
+    check(
+      (ints: Array[Int], item: Int) => {
+        val intSet = ints.toSet + item
+        val compact = Ints.create(ints)
+        IntSet(IntSet(compact).insert(item)).toSet == intSet
+      },
+      minSuccessful(1000))
   }
 
 }
