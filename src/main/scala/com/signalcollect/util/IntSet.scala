d5c0e287fbe2d2af3df6e7b663adf729331c407d
Philip Stutz <philip.stutz@gmail.com>
2013-11-25 18:30:35 +0100

    Manually inlining foreach traversal inside the IntSet.insert operation.

--- src/main/scala/com/signalcollect/util/IntSet.scala
+++ src/main/scala/com/signalcollect/util/IntSet.scala
@@ -46,26 +46,42 @@ class IntSet(val encoded: Array[Byte]) extends AnyVal {
 
   /**
    * Inserts item into the set.
+   * TODO: Determine insert index, compute extra space, splice in inserted element, adjust next delta
+   * and use arraycopy twice to perform insert. Should be faster, but would be tedious to implement.
    */
   def insert(item: Int): Array[Byte] = {
     val baos = new ByteArrayOutputStream
     val dos = new DataOutputStream(baos)
     var itemPresentOrWritten = false
     var previous = -1
-    foreach(current => {
-      if (!itemPresentOrWritten) {
-        if (current < item) {
-        } else if (current == item) {
-          itemPresentOrWritten = true
-        } else if (current > item) {
-          writeUnsignedVarInt(item - previous - 1, dos)
-          previous = item
-          itemPresentOrWritten = true
+    // Inlining manually to ensure there is no boxing.
+    var i = 0
+    var previousInt = -1
+    var currentInt = 0
+    var shift = 0
+    while (i < encoded.length) {
+      val readByte = encoded(i)
+      currentInt |= (readByte & leastSignificant7BitsMask) << shift
+      shift += 7
+      if ((readByte & hasAnotherByte) == 0) {
+        // Next byte is no longer part of this Int.
+        previousInt += currentInt + 1
+        if (!itemPresentOrWritten) {
+          if (previousInt == item) {
+            itemPresentOrWritten = true
+          } else if (previousInt > item) {
+            writeUnsignedVarInt(item - previous - 1, dos)
+            previous = item
+            itemPresentOrWritten = true
+          }
         }
+        writeUnsignedVarInt(previousInt - previous - 1, dos)
+        previous = previousInt
+        currentInt = 0
+        shift = 0
       }
-      writeUnsignedVarInt(current - previous - 1, dos)
-      previous = current
-    })
+      i += 1
+    }
     if (!itemPresentOrWritten) {
       writeUnsignedVarInt(item - previous - 1, dos)
     }
@@ -86,6 +102,7 @@ class IntSet(val encoded: Array[Byte]) extends AnyVal {
     var startingIndexOfCurrentInt = 0
     var currentInt = 0
     var shift = 0
+    // Inlining manually to ensure there is no boxing.
     while (i < encoded.length) {
       val readByte = encoded(i)
       currentInt |= (readByte & leastSignificant7BitsMask) << shift
@@ -107,7 +124,7 @@ class IntSet(val encoded: Array[Byte]) extends AnyVal {
     notFound
   }
 
-  def foreach[U](f: Int => U) {
+  @inline def foreach(f: Int => Unit) {
     var i = 0
     var previousInt = -1
     var currentInt = 0
