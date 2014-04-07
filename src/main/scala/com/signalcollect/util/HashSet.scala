279e808997a35dd9bef01800ba15f1bbcaaf6e2d
Philip Stutz <philip.stutz@gmail.com>
2014-04-07 14:04:18 +0200

    Added custom HashSet.

---
+++ src/main/scala/com/signalcollect/util/HashSet.scala
@@ -0,0 +1,190 @@
+/*
+ *  @author Philip Stutz
+ *
+ *  Copyright 2014 University of Zurich
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.signalcollect.util
+
+import scala.Array.canBuildFrom
+import scala.reflect.ClassTag
+
+class HashSet[Key <: AnyRef: ClassTag](
+  initialSize: Int = 32768,
+  rehashFraction: Float = 0.75f) {
+  assert(initialSize > 0)
+  final var maxSize = nextPowerOfTwo(initialSize)
+  assert(1.0f >= rehashFraction && rehashFraction > 0.1f, "Unreasonable rehash fraction.")
+  assert(maxSize > 0 && maxSize >= initialSize, "Initial size is too large.")
+  private[this] final var maxElements: Int = (rehashFraction * maxSize).floor.toInt
+  private[this] final var keys = new Array[Key](maxSize) // 0 means empty
+  private[this] final var mask = maxSize - 1
+  private[this] final var nextPositionToProcess = 0
+
+  final def size: Int = numberOfElements
+  final def isEmpty: Boolean = numberOfElements == 0
+  private[this] final var numberOfElements = 0
+
+  final def clear {
+    keys = new Array[Key](maxSize)
+    numberOfElements = 0
+    nextPositionToProcess = 0
+  }
+
+  def toScalaSet: Set[Key] = {
+    keys.filter(_ != null).toSet
+  }
+
+  private[this] final def tryDouble {
+    // 1073741824 is the largest size and cannot be doubled anymore.
+    if (maxSize != 1073741824) {
+      val oldKeys = keys
+      val oldNumberOfElements = numberOfElements
+      maxSize *= 2
+      maxElements = (rehashFraction * maxSize).floor.toInt
+      keys = new Array[Key](maxSize)
+      mask = maxSize - 1
+      numberOfElements = 0
+      var i = 0
+      var elementsMoved = 0
+      while (elementsMoved < oldNumberOfElements) {
+        val oldKey = oldKeys(i)
+        if (oldKey != null) {
+          add(oldKey)
+          elementsMoved += 1
+        }
+        i += 1
+      }
+    }
+  }
+
+  final def foreach(f: Key => Unit) {
+    var i = 0
+    var elementsProcessed = 0
+    while (elementsProcessed < numberOfElements) {
+      val key = keys(i)
+      if (key != null) {
+        f(key)
+        elementsProcessed += 1
+      }
+      i += 1
+    }
+  }
+
+  final def remove(key: Key) {
+    remove(key, true)
+  }
+
+  private final def remove(key: Key, optimize: Boolean) {
+    var position = keyToPosition(key)
+    var keyAtPosition = keys(position)
+    while (keyAtPosition != null && key != keyAtPosition) {
+      position = (position + 1) & mask
+      keyAtPosition = keys(position)
+    }
+    // We can only remove the entry if it was found.
+    if (keyAtPosition != null) {
+      keys(position) = null.asInstanceOf[Key]
+      numberOfElements -= 1
+      if (optimize) {
+        optimizeFromPosition((position + 1) & mask)
+      }
+    }
+  }
+
+  // Try to reinsert all elements that are not optimally placed until an empty position is found.
+  // See http://stackoverflow.com/questions/279539/best-way-to-remove-an-entry-from-a-hash-table
+  private[this] final def optimizeFromPosition(startingPosition: Int) {
+    var currentPosition = startingPosition
+    var keyAtPosition = keys(currentPosition)
+    while (isCurrentPositionOccupied) {
+      val perfectPositionForEntry = keyToPosition(keyAtPosition)
+      if (perfectPositionForEntry != currentPosition) {
+        // We try to optimize the placement of the entry by removing and then reinserting it.
+        removeCurrentEntry
+        add(keyAtPosition)
+      }
+      advance
+    }
+    @inline def advance {
+      currentPosition = ((currentPosition + 1) & mask)
+      keyAtPosition = keys(currentPosition)
+    }
+    @inline def isCurrentPositionOccupied = {
+      keyAtPosition != null
+    }
+    @inline def removeCurrentEntry {
+      keys(currentPosition) = null.asInstanceOf[Key]
+      numberOfElements -= 1
+    }
+  }
+
+  final def apply(key: Key) = contains(key)
+
+  @inline final def contains(key: Key): Boolean = {
+    var position = keyToPosition(key)
+    var keyAtPosition = keys(position)
+    while (keyAtPosition != null && key != keyAtPosition) {
+      position = (position + 1) & mask
+      keyAtPosition = keys(position)
+    }
+    if (keyAtPosition != null) {
+      true
+    } else {
+      false
+    }
+  }
+
+  /**
+   * Returns true iff this entry already existed.
+   */
+  def add(key: Key): Boolean = {
+    assert(key != null, "Key cannot be null")
+    var position = keyToPosition(key)
+    var keyAtPosition = keys(position)
+    while (keyAtPosition != null && key != keyAtPosition) {
+      position = (position + 1) & mask
+      keyAtPosition = keys(position)
+    }
+    val alreadyContained = keyAtPosition == key
+    if (!alreadyContained) {
+      keys(position) = key
+      numberOfElements += 1
+      if (numberOfElements >= maxElements) {
+        tryDouble
+        if (numberOfElements >= maxSize) {
+          throw new OutOfMemoryError("The hash map is full and cannot be expanded any further.")
+        }
+      }
+      add(key)
+    }
+    alreadyContained
+  }
+
+  private[this] final def keyToPosition(key: Key) = {
+    key.hashCode & mask
+  }
+
+  private[this] final def nextPowerOfTwo(x: Int): Int = {
+    var r = x - 1
+    r |= r >> 1
+    r |= r >> 2
+    r |= r >> 4
+    r |= r >> 8
+    r |= r >> 16
+    r + 1
+  }
+
+}
---
+++ src/test/scala/com/signalcollect/util/HashSetSpec.scala
@@ -0,0 +1,82 @@
+/*
+ *  @author Philip Stutz
+ *
+ *  Copyright 2014 University of Zurich
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.signalcollect.util
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbContainer
+import org.scalacheck.Gen
+import org.scalatest.FlatSpec
+import org.scalatest.prop.Checkers
+
+class HasSetSpec extends FlatSpec with Checkers {
+
+  "HasSet" should "support inserts" in {
+    check(
+      (setEntries: Array[String]) => {
+        var wasEqual = false
+        val s = new HashSet[String](8, 0.5f)
+        try {
+          val sCorrect = setEntries.toSet
+          for (e <- setEntries) {
+            s.add(e)
+          }
+          wasEqual = s.toScalaSet == sCorrect
+          if (!wasEqual) {
+            println("Problematic set: " + s.toScalaSet +
+              "\nShould have been: " + sCorrect.toString)
+          }
+        } catch {
+          case t: Throwable =>
+            t.printStackTrace
+        }
+        wasEqual
+      },
+      minSuccessful(1000))
+  }
+
+  it should "support removals" in {
+    check(
+      (toAdd: Array[String], toAddThenRemove: Array[String]) => {
+        val setEntries = toAdd.toSet ++ toAddThenRemove.toSet
+        var wasEqual = false
+        var sCorrect = setEntries.toSet
+        val s = new HashSet[String](8, 0.5f)
+        try {
+          for (e <- setEntries) {
+            s.add(e)
+          }
+          for (e <- toAddThenRemove) {
+            s.remove(e)
+            sCorrect -= e
+          }
+          wasEqual = s.toScalaSet == sCorrect
+          if (!wasEqual) {
+            println("Problematic set: " + s.toScalaSet +
+              "\nShould have been: " + sCorrect.toString)
+          }
+        } catch {
+          case t: Throwable =>
+            t.printStackTrace
+        }
+        wasEqual
+      },
+      minSuccessful(1000))
+  }
+
+}
