468b82c7fc99fc4d961afa9397568a6b41c31a50
Philip Stutz <philip.stutz@gmail.com>
2014-07-29 10:44:43 +0200

    Enabling throttling during graph loading.

--- src/main/scala/com/signalcollect/worker/AkkaWorker.scala
+++ src/main/scala/com/signalcollect/worker/AkkaWorker.scala
@@ -308,7 +308,7 @@ class AkkaWorker[@specialized(Int, Long) Id: ClassTag, Signal: ClassTag](
           val overloaded = throttlingEnabled && (
             pongDelayed || worker.slowPongDetected)
           //          log.debug(s"Worker $workerId has work to do")
-          if (worker.isPaused) { // && !overloaded
+          if (worker.isPaused && !overloaded) { // && !overloaded
             //            log.debug(s"Worker $workerId is paused. Pending worker operations: ${!worker.pendingModifications.isEmpty}")
             applyPendingGraphModifications
           } else {
