505b6bf39e2b804ef1c8934a9e697486e93281c9
Philip Stutz <philip.stutz@gmail.com>
2014-08-27 13:30:24 +0200

    Moved iterator to separate class.

--- src/main/scala/com/signalcollect/util/FileReader.scala
+++ src/main/scala/com/signalcollect/util/FileReader.scala
@@ -30,52 +30,7 @@ object FileReader {
    * single spaces and single newlines.
    */
   def intIterator(filePath: String): Iterator[Int] = {
-    new Iterator[Int] {
-      var initialized = false
-      val BLOCK_SIZE = 8 * 32768
-      var in: BufferedInputStream = _
-      var currentNumber = 0
-      var inputIndex = 0
-      var read: Int = _
-      var buf: Array[Byte] = _
-
-      def next: Int = {
-        currentNumber
-      }
-
-      def hasNext: Boolean = {
-        if (!initialized) initialize
-        currentNumber = readNext
-        currentNumber != -1
-      }
-
-      def readNext: Int = {
-        currentNumber = 0
-        while (read != -1) {
-          while (inputIndex < read) {
-            val b = buf(inputIndex)
-            if (b != ' ' && b != '\n') {
-              currentNumber = 10 * currentNumber + b - '0'
-            } else {
-              inputIndex += 1
-              return currentNumber
-            }
-            inputIndex += 1
-          }
-          inputIndex -= read
-          read = in.read(buf)
-        }
-        in.close
-        return -1
-      }
-
-      def initialize {
-        in = new BufferedInputStream(new FileInputStream(filePath))
-        buf = new Array[Byte](BLOCK_SIZE)
-        read = in.read(buf)
-      }
-
-    }
+    new AsciiIntIterator(filePath)
   }
 
   /**
@@ -109,4 +64,50 @@ object FileReader {
     in.close
   }
 
+}
+
+final private class AsciiIntIterator(filePath: String) extends Iterator[Int] {
+  var initialized = false
+  final val BLOCK_SIZE = 8 * 32768
+  var in: BufferedInputStream = _
+  var currentNumber = 0
+  var inputIndex = 0
+  var read: Int = _
+  var buf: Array[Byte] = _
+
+  @inline def next: Int = {
+    currentNumber
+  }
+
+  @inline def hasNext: Boolean = {
+    if (!initialized) initialize
+    currentNumber = readNext
+    currentNumber != -1
+  }
+
+  @inline def readNext: Int = {
+    currentNumber = 0
+    while (read != -1) {
+      while (inputIndex < read) {
+        val b = buf(inputIndex)
+        if (b != ' ' && b != '\n') {
+          currentNumber = 10 * currentNumber + b - '0'
+        } else {
+          inputIndex += 1
+          return currentNumber
+        }
+        inputIndex += 1
+      }
+      inputIndex -= read
+      read = in.read(buf)
+    }
+    in.close
+    return -1
+  }
+
+  def initialize {
+    in = new BufferedInputStream(new FileInputStream(filePath))
+    buf = new Array[Byte](BLOCK_SIZE)
+    read = in.read(buf)
+  }
 }
