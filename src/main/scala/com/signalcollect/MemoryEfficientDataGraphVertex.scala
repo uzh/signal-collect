326ce85a45403db76e2e9081505a5ceb7e62cfb3
Philip Stutz <philip.stutz@gmail.com>
2014-10-22 11:16:34 +0200

    Can set graph signal type for data graph vertex.

--- src/main/scala/com/signalcollect/MemoryEfficientDataGraphVertex.scala
+++ src/main/scala/com/signalcollect/MemoryEfficientDataGraphVertex.scala
@@ -32,15 +32,13 @@ import com.signalcollect.util.MemoryEfficientSplayIntSet
  * The signal function is the same for all edges and defined in
  * the 'computeSignal' function.
  */
-abstract class MemoryEfficientDataGraphVertex[State, IncomingSignalType: ClassTag](
+abstract class MemoryEfficientDataGraphVertex[State, IncomingSignalType: ClassTag, GraphSignalType](
   val id: Int,
-  var state: State) extends Vertex[Int, State, Int, Any] {
-
-  type OutgoingSignalType
+  var state: State) extends Vertex[Int, State, Int, GraphSignalType] {
 
   def collect: State
 
-  def computeSignal(targetId: Int): OutgoingSignalType
+  def computeSignal(targetId: Int): GraphSignalType
 
   var lastSignalState: State = null.asInstanceOf[State]
 
@@ -60,17 +58,17 @@ abstract class MemoryEfficientDataGraphVertex[State, IncomingSignalType: ClassTa
 
   val mostRecentSignalMap = new IntHashMap[IncomingSignalType](1, 0.85f)
 
-  override def deliverSignalWithSourceId(signal: Any, sourceId: Int, graphEditor: GraphEditor[Int, Any]): Boolean = {
+  override def deliverSignalWithSourceId(signal: GraphSignalType, sourceId: Int, graphEditor: GraphEditor[Int, GraphSignalType]): Boolean = {
     val s = signal.asInstanceOf[IncomingSignalType]
     mostRecentSignalMap.put(sourceId, s)
     false
   }
 
-  override def deliverSignalWithoutSourceId(signal: Any, graphEditor: GraphEditor[Int, Any]): Boolean = {
+  override def deliverSignalWithoutSourceId(signal: GraphSignalType, graphEditor: GraphEditor[Int, GraphSignalType]): Boolean = {
     throw new Exception(s"A data graph vertex requires the sender ID for signals. Vertex ${this.toString} just received signal ${signal} without a sender ID.")
   }
 
-  override def executeSignalOperation(graphEditor: GraphEditor[Int, Any]) {
+  override def executeSignalOperation(graphEditor: GraphEditor[Int, GraphSignalType]) {
     _targetIds.foreach { targetId =>
       graphEditor.sendSignal(computeSignal(targetId), targetId, id)
     }
@@ -81,19 +79,19 @@ abstract class MemoryEfficientDataGraphVertex[State, IncomingSignalType: ClassTa
 
   override def toString = s"${this.getClass.getName}(state=$state)"
 
-  def executeCollectOperation(graphEditor: GraphEditor[Int, Any]) {
+  def executeCollectOperation(graphEditor: GraphEditor[Int, GraphSignalType]) {
     setState(collect)
   }
 
-  override def addEdge(e: Edge[Int], graphEditor: GraphEditor[Int, Any]): Boolean = {
+  override def addEdge(e: Edge[Int], graphEditor: GraphEditor[Int, GraphSignalType]): Boolean = {
     _targetIds.insert(e.targetId.asInstanceOf[Int])
   }
 
-  override def removeEdge(targetId: Int, graphEditor: GraphEditor[Int, Any]): Boolean = throw new UnsupportedOperationException
+  override def removeEdge(targetId: Int, graphEditor: GraphEditor[Int, GraphSignalType]): Boolean = throw new UnsupportedOperationException
 
-  override def removeAllEdges(graphEditor: GraphEditor[Int, Any]): Int = throw new UnsupportedOperationException
+  override def removeAllEdges(graphEditor: GraphEditor[Int, GraphSignalType]): Int = throw new UnsupportedOperationException
 
-  def afterInitialization(graphEditor: GraphEditor[Int, Any]) = {}
+  def afterInitialization(graphEditor: GraphEditor[Int, GraphSignalType]) = {}
 
-  def beforeRemoval(graphEditor: GraphEditor[Int, Any]) = {}
+  def beforeRemoval(graphEditor: GraphEditor[Int, GraphSignalType]) = {}
 }
