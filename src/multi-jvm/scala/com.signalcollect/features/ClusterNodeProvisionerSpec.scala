1110169bbab29d062a31e56232c6e7956c528da9
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2015-08-31 14:23:38 +0530

    A simple multi-jvm spec

--- src/multi-jvm/scala/com.signalcollect/features/ClusterNodeProvisionerSpec.scala
+++ src/multi-jvm/scala/com.signalcollect/features/ClusterNodeProvisionerSpec.scala
@@ -18,41 +18,70 @@
 
 package com.signalcollect.features
 
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent.{CurrentClusterState, MemberUp}
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec, MultiNodeSpecCallbacks}
 import akka.testkit.ImplicitSender
 import com.signalcollect.GraphBuilder
 import com.signalcollect.nodeprovisioning.cluster.ClusterNodeProvisioner
 import com.signalcollect.util.TestAnnouncements
+import com.typesafe.config.ConfigFactory
 import org.scalatest._
 
-trait STMultiNodeSpec extends MultiNodeSpecCallbacks with WordSpecLike with Matchers with BeforeAndAfterAll {
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+trait STMultiNodeSpec extends MultiNodeSpecCallbacks with WordSpecLike with ShouldMatchers with BeforeAndAfterAll {
 
   override def beforeAll() = multiNodeSpecBeforeAll()
 
   override def afterAll() = multiNodeSpecAfterAll()
 }
 
+class ClusterNodeProvisionerSpecMultiJvmNode1 extends ClusterNodeProvisionerSpec
+
+class ClusterNodeProvisionerSpecMultiJvmNode2 extends ClusterNodeProvisionerSpec
+
+class ClusterNodeProvisionerSpecMultiJvmNode3 extends ClusterNodeProvisionerSpec
+
 object MultiNodeTestConfig extends MultiNodeConfig {
-  val node1 = role("node1")
+  val master = role("master")
+  val worker1 = role("worker1")
+  val worker2 = role("worker2")
+
+  // this configuration will be used for all nodes
+  // note that no fixed host names and ports are used
+  commonConfig(ConfigFactory.load())
 }
 
-class ClusterNodeProvisionerSpecMultiJvmNode1 extends MultiNodeSpec(MultiNodeTestConfig) with STMultiNodeSpec
+class ClusterNodeProvisionerSpec extends MultiNodeSpec(MultiNodeTestConfig) with STMultiNodeSpec
 with ImplicitSender with TestAnnouncements {
 
   import MultiNodeTestConfig._
 
   override def initialParticipants = roles.size
 
-  "Clustered Signal/Collect" should {
+  val masterAddress = node(master).address
+  val worker1Address = node(worker1).address
+  val worker2Address = node(worker2).address
+  val workers = 2
 
-    "wait for all nodes to enter a barrier" in {
-      testConductor.enter("startup")
+  muteDeadLetters(classOf[Any])(system)
+
+  "SignalCollect" should {
+
+    "form the cluster" in within(300 seconds) {
+      Cluster(system).subscribe(testActor, classOf[MemberUp])
+      expectMsgClass(classOf[CurrentClusterState])
+
+      Cluster(system).join(masterAddress)
+      receiveN(3).map { case MemberUp(m) => m.address }.toSet should be(Set(masterAddress, worker1Address, worker2Address))
+      Cluster(system).unsubscribe(testActor)
+      testConductor.enter("cluster-up")
     }
 
     "support setting the number of workers created on each node" in {
-      runOn(node1) {
-        testConductor.enter("all deployed")
-        val workers = 2
+      runOn(master) {
         val graph = GraphBuilder.withNodeProvisioner(new ClusterNodeProvisioner(numberOfNodes = workers)).build
         try {
           val stats = graph.execute
