36e6a00e6010e0d9b32500e75164534c441f63cf
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2015-09-03 14:38:48 +0530

    Minor refactoring

--- src/multi-jvm/scala/com.signalcollect/features/ClusterNodeProvisionerSpec.scala
+++ src/multi-jvm/scala/com.signalcollect/features/ClusterNodeProvisionerSpec.scala
@@ -18,49 +18,45 @@
 
 package com.signalcollect.features
 
+import akka.actor.{ActorRef, Props}
 import akka.cluster.Cluster
+import akka.pattern.ask
 import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
 import akka.testkit.ImplicitSender
-import com.signalcollect.nodeprovisioning.cluster.{ClusterNodeEntryPointTemplate, ClusterNodeProvisioner}
+import akka.util.Timeout
+import com.signalcollect.nodeprovisioning.cluster.{ClusterNodeProvisionerActor, RetrieveNodeActors}
 import com.signalcollect.{Graph, GraphBuilder, STMultiNodeSpec}
 import com.typesafe.config.ConfigFactory
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
+
 import scala.concurrent.duration._
 
 class ClusterNodeProvisionerSpecMultiJvmNode1 extends ClusterNodeProvisionerSpec
 
 class ClusterNodeProvisionerSpecMultiJvmNode2 extends ClusterNodeProvisionerSpec
 
-//class ClusterNodeProvisionerSpecMultiJvmNode3 extends ClusterNodeProvisionerSpec
+class ClusterNodeProvisionerSpecMultiJvmNode3 extends ClusterNodeProvisionerSpec
 
 object ClusterNodeProvisionerConfig extends MultiNodeConfig {
   val provisioner = role("provisioner")
   val node1 = role("node1")
-//  val node2 = role("node2")
+  val node2 = role("node2")
+
   val nodeConfig = ConfigFactory.load()
   val seedIp = nodeConfig.getString("akka.clustering.seed-ip")
   val seedPort = nodeConfig.getInt("akka.clustering.seed-port")
   val clusterName = "ClusterNodeProvisionerSpec"
 
-  val provisionerChildConfig = {
+  nodeConfig(provisioner) {
     ConfigFactory.parseString(
-      s"""akka.clustering.name=$clusterName
-          |akka.cluster.seed-nodes=["akka.tcp://$clusterName@$seedIp:$seedPort"]
-          |akka.remote.netty.tcp.port=$seedPort""".stripMargin).withFallback(ConfigFactory.load())
+      s"""akka.remote.netty.tcp.port=$seedPort""".stripMargin)
   }
 
-  val workerNodeConfig = {
-    ConfigFactory.parseString(
-      s"""akka.clustering.name=$clusterName
-          |akka.cluster.seed-nodes=["akka.tcp://$clusterName@$seedIp:$seedPort"]
-          |akka.cluster.roles = [node]
-       """.stripMargin).withFallback(ConfigFactory.load())
-  }
-
-  nodeConfig(node1) {
-      workerNodeConfig
-    }
+  // this configuration will be used for all nodes
+  commonConfig(ConfigFactory.parseString(
+    s"""akka.cluster.seed-nodes=["akka.tcp://"${clusterName}"@"${seedIp}":"${seedPort}]""".stripMargin)
+    .withFallback(ConfigFactory.load()))
 }
 
 class ClusterNodeProvisionerSpec extends MultiNodeSpec(ClusterNodeProvisionerConfig) with STMultiNodeSpec
@@ -70,38 +66,56 @@ with ImplicitSender with ScalaFutures {
 
   override def initialParticipants = roles.size
 
+  override def atStartup() = println("STARTING UP!")
+
+  override def afterTermination() = println("TERMINATION!")
+
   implicit override val patienceConfig =
     PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(1000, Millis)))
 
-  val workers = 2
+  val workers = 3
+  val node1Address = node(node1).address
+  val node2Address = node(node2).address
   val idleDetectionPropagationDelayInMilliseconds = 500
-
-  var graph: Graph[Any, Any] = _
-
-  "Signal-Collect" must {
-    "start provisioner node" in within(100.seconds){
+  
+  "SignalCollect" should {
+    "get the cluster up" in {
       runOn(provisioner) {
-        assert(!provisionerChildConfig.toString.contains("SignalCollect"))
-        graph = GraphBuilder.withActorSystem(system).withNodeProvisioner(new ClusterNodeProvisioner(numberOfNodes = workers,
-          config = provisionerChildConfig)).build
+        system.actorOf(Props(classOf[ClusterNodeProvisionerActor], idleDetectionPropagationDelayInMilliseconds,
+          "ClusterMasterBootstrap", workers), "ClusterMasterBootstrap")
       }
       enterBarrier("provisioner up")
 
       runOn(node1) {
-        ClusterNodeEntryPointTemplate.startWithActorSystemName(clusterName, workerNodeConfig)
+        Cluster(system).join(node1Address)
       }
-      enterBarrier("node1 up")
+      enterBarrier("node1 started")
+
+      runOn(node2) {
+        Cluster(system).join(node2Address)
+      }
+      enterBarrier("node2 started")
 
       runOn(provisioner) {
-        println("--- asserting")
-        try {
-          val stats = graph.execute
-          assert(stats.individualWorkerStatistics.length == 4 * workers)
-        } finally {
-          graph.shutdown
+        implicit val timeout = Timeout(300.seconds)
+        val masterActor = system.actorSelection(node(provisioner) / "user" / "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.size == workers)
+
+          val graph = GraphBuilder.withActorSystem(system).withPreallocatedNodes(nodeActors).build
+          try {
+            val stats = graph.execute
+            assert(stats.individualWorkerStatistics.length == Runtime.getRuntime.availableProcessors * workers)
+          } finally {
+            graph.shutdown
+          }
         }
       }
-      enterBarrier("all done")
+      enterBarrier("all nodes up!")
     }
   }
 }
+
+
+
