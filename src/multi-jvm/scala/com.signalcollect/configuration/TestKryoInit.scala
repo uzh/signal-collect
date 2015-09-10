8aa0189a9c0183f5554e97c0762715f4d05623cc
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2015-09-10 14:06:32 +0530

    Removing not required registrations

--- src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
+++ src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
@@ -7,8 +7,6 @@ class TestKryoInit extends KryoInit {
     kryo.setReferences(true)
     kryo.setCopyReferences(true)
     kryo.register(Class.forName("com.signalcollect.ClusterIntegrationSpec$$anonfun$test$1$$anonfun$apply$1$$anon$1"), 2037610320)
-    kryo.register(Class.forName("com.signalcollect.examples.PageRankEdge"), 2037610321)
-    kryo.register(Class.forName("com.signalcollect.examples.PageRankVertex"), 2037610322)
     register(kryo)
   }
 }
