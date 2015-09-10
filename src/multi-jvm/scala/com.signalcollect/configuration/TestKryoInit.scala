778cee23f6c979a195ce2d100b6b032553b8c810
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2015-09-10 13:44:41 +0530

    Registering a class with id to avoid class not registered exception

--- src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
+++ src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
@@ -2,26 +2,13 @@ package com.signalcollect.configuration
 
 import com.esotericsoftware.kryo.Kryo
 
-class TestKryoInit1 extends KryoInit {
+class TestKryoInit extends KryoInit {
   override def customize(kryo: Kryo): Unit = {
     kryo.setReferences(true)
     kryo.setCopyReferences(true)
-    register(kryo)
-  }
-}
-
-class TestKryoInit2 extends KryoInit {
-  override def customize(kryo: Kryo): Unit = {
-    kryo.setReferences(true)
-    kryo.setCopyReferences(true)
-    register(kryo)
-  }
-}
-
-class TestKryoInit3 extends KryoInit {
-  override def customize(kryo: Kryo): Unit = {
-    kryo.setReferences(true)
-    kryo.setCopyReferences(true)
+    kryo.register(Class.forName("com.signalcollect.ClusterIntegrationSpec$$anonfun$test$1$$anonfun$apply$1$$anon$1"), 2037610320)
+    kryo.register(Class.forName("com.signalcollect.examples.PageRankEdge"), 2037610321)
+    kryo.register(Class.forName("com.signalcollect.examples.PageRankVertex"), 2037610322)
     register(kryo)
   }
 }
