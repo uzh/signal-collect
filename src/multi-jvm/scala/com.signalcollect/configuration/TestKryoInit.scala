cda645ff1add616a6b53633b30368e954b11201a
Bharath Kumar <Bharath.Lingannagari@ihealthtechnologies.com>
2015-09-11 16:33:07 +0530

    Having only one kryo instance for tests

--- src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
+++ src/multi-jvm/scala/com.signalcollect/configuration/TestKryoInit.scala
@@ -6,7 +6,6 @@ class TestKryoInit extends KryoInit {
   override def customize(kryo: Kryo): Unit = {
     kryo.setReferences(true)
     kryo.setCopyReferences(true)
-    kryo.register(Class.forName("com.signalcollect.ClusterIntegrationSpec$$anonfun$test$1$$anonfun$apply$1$$anon$1"), 2037610320)
     register(kryo)
   }
 }
