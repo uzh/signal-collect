a4c3b0ac4a6afce4dd122009baef82a2cc1a30e6
Philip Stutz <philip.stutz@gmail.com>
2015-09-16 16:10:22 +0200

    Update ClusterIntegrationSpec.scala

--- src/multi-jvm/scala/com.signalcollect/ClusterIntegrationSpec.scala
+++ src/multi-jvm/scala/com.signalcollect/ClusterIntegrationSpec.scala
@@ -96,9 +96,9 @@ with ImplicitSender with ScalaFutures {
 
   val workers = roles.size
 
-  override def atStartup() = println("STARTING UP!")
+  override def atStartup() = println("Starting")
 
-  override def afterTermination() = println("TERMINATION!")
+  override def afterTermination() = println("Terminated")
 
   implicit override val patienceConfig =
     PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(1000, Millis)))
