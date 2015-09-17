b50872b3e1048d7c8f1f75687e53781ed36c0117
sunnylbk <sunny.lbk@gmail.com>
2015-09-17 18:51:19 +0530

    Splitting larger ClusterIntegrationSpec into multiple smaller specs

---
+++ src/multi-jvm/scala/com.signalcollect/ClusterSSSPSpec.scala
@@ -0,0 +1,153 @@
+package com.signalcollect
+
+import akka.actor.{ActorRef, Props}
+import akka.event.Logging
+import akka.pattern.ask
+import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
+import akka.testkit.ImplicitSender
+import akka.util.Timeout
+import com.signalcollect.ClusterTestUtils._
+import com.signalcollect.configuration.Akka
+import com.signalcollect.examples.{Location, Path}
+import com.signalcollect.nodeprovisioning.cluster.{ClusterNodeProvisionerActor, RetrieveNodeActors}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+
+import scala.concurrent.duration._
+
+class ClusterSSSPSpecMultiJvmNode1 extends ClusterSSSPSpec
+
+class ClusterSSSPSpecMultiJvmNode2 extends ClusterSSSPSpec
+
+class ClusterSSSPSpecMultiJvmNode3 extends ClusterSSSPSpec
+
+object ClusterSSSPConfig extends MultiNodeConfig {
+  val provisioner = role("provisioner")
+  val node1 = role("node1")
+  val node2 = role("node2")
+
+  val clusterName = "ClusterSSSPSpec"
+  val seedPort = 2560
+
+  val akkaConfig = Akka.config(serializeMessages = Some(false),
+    loggingLevel = Some(Logging.WarningLevel),
+    kryoRegistrations = List.empty,
+    kryoInitializer = Some("com.signalcollect.configuration.TestKryoInit"))
+
+  nodeConfig(provisioner) {
+    TestClusterConfig.provisionerCommonConfig(seedPort)
+  }
+
+  commonConfig {
+    val mappingsConfig =
+      """akka.actor.kryo.mappings {
+        |  "com.signalcollect.ModularAggregator" = 133,
+        |  "com.signalcollect.ClusterSSSPSpec$$anonfun$2" = 134,
+        |  "com.signalcollect.ClusterSSSPSpec$$anonfun$3" = 135
+        |    }""".stripMargin
+    ConfigFactory.parseString(
+      s"""akka.actor.kryo.idstrategy=incremental
+          |akka.testconductor.barrier-timeout=60s
+       """.stripMargin)
+      .withFallback(TestClusterConfig.nodeCommonConfig(clusterName, seedPort))
+      .withFallback(ConfigFactory.parseString(mappingsConfig))
+      .withFallback(akkaConfig)
+  }
+}
+
+class ClusterSSSPSpec extends MultiNodeSpec(ClusterSSSPConfig) with STMultiNodeSpec
+with ImplicitSender with ScalaFutures {
+
+  import ClusterSSSPConfig._
+
+  override def initialParticipants = roles.size
+
+  val workers = roles.size
+
+  override def atStartup() = println("Starting")
+
+  override def afterTermination() = println("Terminated")
+
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(1000, Millis)))
+
+  val ssspSymmetricsFourCycleVerifier: Vertex[_, _, _, _] => Boolean = v => {
+    val state = v.state.asInstanceOf[Option[Int]].get
+    val expectedState = v.id
+    val correct = state == expectedState
+    if (!correct) {
+      System.err.println("Problematic vertex:  id=" + v.id + ", expected state=" + expectedState + ", actual state=" + state)
+    }
+    correct
+  }
+
+  val ssspSymmetricFiveStarVerifier: Vertex[_, _, _, _] => Boolean = v => {
+    val state = v.state.asInstanceOf[Option[Int]].get
+    val expectedState = if (v.id == 4) 0 else 1
+    val correct = state == expectedState
+    if (!correct) {
+      System.err.println("Problematic vertex:  id=" + v.id + ", expected state=" + expectedState + ", actual state=" + state)
+    }
+    correct
+  }
+
+  def buildSsspGraph(pathSourceId: Any, graph: Graph[Any, Any], edgeTuples: Traversable[Tuple2[Int, Int]]): Graph[Any, Any] = {
+    edgeTuples foreach {
+      case (sourceId, targetId) =>
+        if (sourceId.equals(pathSourceId)) {
+          graph.addVertex(new Location(sourceId, Some(0)))
+        } else {
+          graph.addVertex(new Location(sourceId, None))
+        }
+        if (targetId.equals(pathSourceId)) {
+          graph.addVertex(new Location(targetId, Some(0)))
+        } else {
+          graph.addVertex(new Location(targetId, None))
+        }
+        graph.addEdge(sourceId, new Path(targetId))
+    }
+    graph
+  }
+
+  "SSSP algorithm" should {
+    implicit val timeout = Timeout(30.seconds)
+
+    "deliver correct results on a symmetric 4-cycle" in {
+      val prefix = TestConfig.prefix
+      val symmetricFourCycleEdges = List((0, 1), (1, 2), (2, 3), (3, 0))
+      runOn(provisioner) {
+        val masterActor = system.actorOf(Props(classOf[ClusterNodeProvisionerActor], TestClusterConfig.idleDetectionPropagationDelayInMilliseconds,
+          prefix, workers), "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.length == workers)
+          val computeGraphFactories: List[() => Graph[Any, Any]] = List(() => GraphBuilder.withActorSystem(system)
+            .withPreallocatedNodes(nodeActors).build)
+          test(graphProviders = computeGraphFactories, verify = ssspSymmetricsFourCycleVerifier, buildGraph = buildSsspGraph(0, _, symmetricFourCycleEdges)) shouldBe true
+        }
+      }
+      enterBarrier("SSSP - test1 done")
+    }
+    TestConfig.printStats()
+
+    "deliver correct results on a symmetric 5-star" in {
+      val prefix = TestConfig.prefix
+      val symmetricFiveStarEdges = List((0, 4), (4, 0), (1, 4), (4, 1), (2, 4), (4, 2), (3, 4), (4, 3))
+      runOn(provisioner) {
+        val masterActor = system.actorOf(Props(classOf[ClusterNodeProvisionerActor], TestClusterConfig.idleDetectionPropagationDelayInMilliseconds,
+          prefix, workers), "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.length == workers)
+          val computeGraphFactories: List[() => Graph[Any, Any]] = List(() => GraphBuilder.withActorSystem(system)
+            .withPreallocatedNodes(nodeActors).build)
+          test(graphProviders = computeGraphFactories, verify = ssspSymmetricFiveStarVerifier, buildGraph = buildSsspGraph(4, _, symmetricFiveStarEdges)) shouldBe true
+        }
+      }
+      enterBarrier("SSSP - test2 done")
+    }
+    TestConfig.printStats()
+  }
+  enterBarrier("SSSP - all tests done")
+}
---
+++ src/multi-jvm/scala/com.signalcollect/ClusterVertexColoringSpec.scala
@@ -0,0 +1,159 @@
+package com.signalcollect
+
+import akka.actor.{ActorRef, Props}
+import akka.event.Logging
+import akka.pattern.ask
+import akka.remote.testkit.{MultiNodeConfig, MultiNodeSpec}
+import akka.testkit.ImplicitSender
+import akka.util.Timeout
+import com.signalcollect.ClusterTestUtils._
+import com.signalcollect.configuration.Akka
+import com.signalcollect.nodeprovisioning.cluster.{RetrieveNodeActors, ClusterNodeProvisionerActor}
+import com.typesafe.config.ConfigFactory
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.time.{Millis, Seconds, Span}
+
+import scala.concurrent.duration._
+
+class ClusterVertexColoringSpecMultiJvmNode1 extends ClusterVertexColoringSpec
+
+class ClusterVertexColoringSpecMultiJvmNode2 extends ClusterVertexColoringSpec
+
+class ClusterVertexColoringSpecMultiJvmNode3 extends ClusterVertexColoringSpec
+
+object ClusterVertexColoringConfig extends MultiNodeConfig {
+  val provisioner = role("provisioner")
+  val node1 = role("node1")
+  val node2 = role("node2")
+
+  val clusterName = "ClusterVertexColoringSpec"
+  val seedPort = 2561
+
+  val akkaConfig = Akka.config(serializeMessages = Some(false),
+    loggingLevel = Some(Logging.WarningLevel),
+    kryoRegistrations = List.empty,
+    kryoInitializer = Some("com.signalcollect.configuration.TestKryoInit"))
+
+  nodeConfig(provisioner) {
+    TestClusterConfig.provisionerCommonConfig(seedPort)
+  }
+
+  commonConfig {
+    val mappingsConfig =
+      """akka.actor.kryo.mappings {
+        |  "com.signalcollect.ModularAggregator" = 133,
+        |  "com.signalcollect.ClusterVertexColoringSpec$$anonfun$2" = 134,
+        |  "com.signalcollect.VerifiedColoredVertex" = 135,
+        |  "com.signalcollect.StateForwarderEdge" = 136
+        |    }""".stripMargin
+    ConfigFactory.parseString(
+      s"""akka.actor.kryo.idstrategy=incremental
+          |akka.testconductor.barrier-timeout=60s
+       """.stripMargin)
+      .withFallback(TestClusterConfig.nodeCommonConfig(clusterName, seedPort))
+      .withFallback(ConfigFactory.parseString(mappingsConfig))
+      .withFallback(akkaConfig)
+  }
+}
+
+class ClusterVertexColoringSpec extends MultiNodeSpec(ClusterVertexColoringConfig) with STMultiNodeSpec
+with ImplicitSender with ScalaFutures {
+
+  import ClusterVertexColoringConfig._
+
+  override def initialParticipants = roles.size
+
+  val workers = roles.size
+
+  override def atStartup() = println("Starting")
+
+  override def afterTermination() = println("Terminated")
+
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = scaled(Span(300, Seconds)), interval = scaled(Span(1000, Millis)))
+
+  val vertexColoringVerifier: Vertex[_, _, _, _] => Boolean = v => {
+    v match {
+      case v: VerifiedColoredVertex =>
+        val verified = !v.publicMostRecentSignals.iterator.contains(v.state)
+        if (!verified) {
+          System.err.println("Vertex Coloring: " + v + " has the same color as one of its neighbors.\n" +
+            "Most recent signals received: " + v.publicMostRecentSignals + "\n" +
+            "Score signal: " + v.scoreSignal)
+        }
+        verified
+      case other =>
+        System.err.println("Vertex " + other + " is not of type VerifiedColoredVertex"); false
+    }
+  }
+
+  def buildVertexColoringGraph(numColors: Int, graph: Graph[Any, Any], edgeTuples: Traversable[Tuple2[Int, Int]]): Graph[Any, Any] = {
+    edgeTuples foreach {
+      case (sourceId, targetId) =>
+        graph.addVertex(new VerifiedColoredVertex(sourceId, numColors))
+        graph.addVertex(new VerifiedColoredVertex(targetId, numColors))
+        graph.addEdge(sourceId, new StateForwarderEdge(targetId))
+    }
+    graph
+  }
+
+  "VertexColoring algorithm" should {
+    implicit val timeout = Timeout(30.seconds)
+
+    "deliver correct results on a symmetric 4-cycle" in {
+      val prefix = TestConfig.prefix
+      val symmetricFourCycleEdges = List((0, 1), (1, 0), (1, 2), (2, 1), (2, 3), (3, 2), (3, 0), (0, 3))
+
+      runOn(provisioner) {
+        val masterActor = system.actorOf(Props(classOf[ClusterNodeProvisionerActor], TestClusterConfig.idleDetectionPropagationDelayInMilliseconds,
+          prefix, workers), "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.length == workers)
+          val computeGraphFactories: List[() => Graph[Any, Any]] = List(() => GraphBuilder.withActorSystem(system)
+            .withPreallocatedNodes(nodeActors).build)
+          test(graphProviders = computeGraphFactories, verify = vertexColoringVerifier, buildGraph = buildVertexColoringGraph(2, _, symmetricFourCycleEdges)) shouldBe true
+        }
+      }
+      enterBarrier("VertexColoring - test1 done")
+    }
+    TestConfig.printStats()
+
+    "deliver correct results on a symmetric 5-star" in {
+      val prefix = TestConfig.prefix
+      val symmetricFiveStarEdges = List((0, 4), (4, 0), (1, 4), (4, 1), (2, 4), (4, 2), (3, 4), (4, 3))
+      runOn(provisioner) {
+        val masterActor = system.actorOf(Props(classOf[ClusterNodeProvisionerActor], TestClusterConfig.idleDetectionPropagationDelayInMilliseconds,
+          prefix, workers), "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.length == workers)
+          val computeGraphFactories: List[() => Graph[Any, Any]] = List(() => GraphBuilder.withActorSystem(system)
+            .withPreallocatedNodes(nodeActors).build)
+          test(graphProviders = computeGraphFactories, verify = vertexColoringVerifier, buildGraph = buildVertexColoringGraph(2, _, symmetricFiveStarEdges)) shouldBe true
+        }
+      }
+      enterBarrier("VertexColoring - test2 done")
+    }
+    TestConfig.printStats()
+
+    "deliver correct results on a 2*2 symmetric grid" in {
+      val prefix = TestConfig.prefix
+      val symmetricTwoOnTwoGridEdges = new Grid(2, 2)
+      runOn(provisioner) {
+        val masterActor = system.actorOf(Props(classOf[ClusterNodeProvisionerActor], TestClusterConfig.idleDetectionPropagationDelayInMilliseconds,
+          prefix, workers), "ClusterMasterBootstrap")
+        val nodeActorsFuture = (masterActor ? RetrieveNodeActors).mapTo[Array[ActorRef]]
+        whenReady(nodeActorsFuture) { nodeActors =>
+          assert(nodeActors.length == workers)
+          val computeGraphFactories: List[() => Graph[Any, Any]] = List(() => GraphBuilder.withActorSystem(system)
+            .withPreallocatedNodes(nodeActors).build)
+          test(graphProviders = computeGraphFactories, verify = vertexColoringVerifier, buildGraph = buildVertexColoringGraph(2, _, symmetricTwoOnTwoGridEdges)) shouldBe true
+        }
+      }
+      enterBarrier("VertexColoring - test3 done")
+    }
+    TestConfig.printStats()
+  }
+  enterBarrier("VertexColoring - all tests done")
+}
