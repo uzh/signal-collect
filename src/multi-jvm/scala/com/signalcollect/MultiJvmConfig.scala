931d91470f075988732245c87477acb56e6b7b30
Philip Stutz <philip.stutz@gmail.com>
2015-09-29 18:57:12 +0200

    Fixed package structure

--- src/multi-jvm/scala/com/signalcollect/MultiJvmConfig.scala
+++ src/multi-jvm/scala/com/signalcollect/MultiJvmConfig.scala
@@ -28,13 +28,14 @@ object MultiJvmConfig {
 
   def provisionerCommonConfig(seedPort: Int) = {
     ConfigFactory.parseString(
-      s"""akka.remote.netty.tcp.port=$seedPort""".stripMargin)
+      s"""akka.remote.netty.tcp.port = $seedPort""".stripMargin)
   }
 
   def nodeCommonConfig(clusterName: String, seedPort: Int, mappingsConfig: String = "") = {
     ConfigFactory.parseString(
-      s"""akka.testconductor.barrier-timeout=60s
-          |akka.cluster.seed-nodes=["akka.tcp://"${clusterName}"@"${seedIp}":"${seedPort}]""".stripMargin)
+      s"""|akka.testconductor.barrier-timeout=60s
+          |akka.cluster.seed-nodes = ["akka.tcp://"${clusterName}"@"${seedIp}":"${seedPort}]
+          |akka.remote.netty.tcp.port = 0""".stripMargin)
       .withFallback(ConfigFactory.load().getConfig("signalcollect"))
       .withFallback(ConfigFactory.parseString(mappingsConfig))
       .withFallback(akkaConfig)
